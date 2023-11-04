f1a46fa754e9d0ef73872cacfabc992ba2cebc5d
Abhinav Chinnusamy <abhinavchinnusamy@gmail.com>
2023-11-05 00:49:24 +0530

    project on hold

--- src/index.js
+++ src/index.js
@@ -1,8 +1,4 @@
 import '@material/web/button/filled-button.js';
 import '@material/web/button/outlined-button.js';
 import '@material/web/checkbox/checkbox.js';
-<<<<<<< Updated upstream
 import '@material/web/all.js';
-=======
-import '@material/web/all.js';
->>>>>>> Stashed changes
